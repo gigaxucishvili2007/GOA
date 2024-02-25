284f6d50c11affe22045719f4a14b2b13f368031
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-02-25 20:39:47 +0400

    commit

---
+++ day.4/day4.py
@@ -0,0 +1,14 @@
+# num_1 = int(input('Enter num_1: '))
+# num_2 = int(input('Enter num_2: '))
+
+# print(num_1 + num_2)
+
+
+#---------------------------------
+
+
+# name = input('Enter your name: ')
+# age = int(input('Enter your age: '))
+# after = int(input('Enter how many years after do you want time travelling: '))
+
+# print(name, 'შენ ხარ', str(age), 'წლის და შენ შეხვდები შენი თავის', str(age + after), 'წლის ვერსიას')
