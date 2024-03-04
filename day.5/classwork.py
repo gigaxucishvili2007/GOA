f58a28b15659b674835761c5131749c2052e6890
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-03-04 20:05:07 +0400

    second commit

---
+++ day.5/classwork.py
@@ -0,0 +1,26 @@
+num1 =int(input("Please enter first number: "))
+num2 = int(input("Please enter second number"))
+
+print(type(num1))
+print(type(num2))
+
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+
+
+print(10 / 5)
+print(10 / 15)
+print(10 / 20)
+print(10 / 25)
+print(10 / 30)
+print(10 / 35)
+print(10 / 40)
+print(10 / 50)
+print(10 / 55)
+print(10 / 60)
+
+
+
+
