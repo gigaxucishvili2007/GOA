5022ca45f70e3bd5587873112a7ca722a1bb72a6
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-03-16 22:38:18 +0400

    second commit

---
+++ day6/lesson6.py
@@ -0,0 +1,48 @@
+print(5 < 6 or 7 > 8)
+print(7 < 5 or 8> 6)
+print(5 < 9 or 7> 4)
+print(10> 5 or 5 > 10)
+print(10==5 or 7!=6)
+
+
+print(10 > 5 and 5 < 10)
+print(14 > 2 and 10 < 20)
+print(12 > 8 and 9 < 13)
+print(18 > 12 and 29 < 38)
+print(28 > 11 and 11 > 19)
+
+
+a=True or False#True
+b= True and False#False
+
+
+a=int(input())
+b=int(input())
+
+print(a and b)
+print(a or b)
+
+
+a=10 > 5
+b= 15 < 20
+c= a or b
+print(c)
+
+
+
+a= 5
+b= ""
+c= 10.15
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+name=input("გამარჯობა რა გქვია შენ")
+group=input("რომელ ჯგუფშიხარ?")
+print("hello",name,"welcome in",group,"group")
+
+
+
