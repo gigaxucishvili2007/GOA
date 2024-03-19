53af86cb616e6196c2717404ec4b94fce8af3979
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-03-19 17:17:51 +0400

    second commit

---
+++ day7/day7.py
@@ -0,0 +1,10 @@
+string = input("enter a string")
+boolean = input("False")
+float = input("30.10")
+integer = input("int")
+
+
+print(type(string))
+print(type(boolean))
+print(type(float))
+print(type(integer))
