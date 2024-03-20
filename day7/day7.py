13ad3b023a09d9a9aa37077110d92e3fb606952a
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2024-03-20 15:55:57 +0400

    second commit

--- day7/day7.py
+++ day7/day7.py
@@ -8,3 +8,26 @@ print(type(string))
 print(type(boolean))
 print(type(float))
 print(type(integer))
+
+
+
+a=30
+b=6 * 5
+c=90 // 3
+d=40 - 10
+e=20 + 10
+
+
+print(a==b==c==d==e)
+
+
+
+print(True and False)
+print(True and True)
+print(False and True)
+print(False and False)
+
+print(True or False)
+print(True or True)
+print(False or False)
+print(False or True)
